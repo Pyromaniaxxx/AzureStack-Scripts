049ee222c3608df27c712ba97e9d54eb6eb7c6b5
Pyromania <s.hiro@hdcr.org>
2017-04-06 22:00:19 +0900

    initial commit

---
+++ Deploy/Set-VHDBoot.ps1
@@ -0,0 +1,75 @@
+#Requires -RunAsAdministrator
+<#
+.Synopsis
+   set vhd boot
+.DESCRIPTION
+   Script to set VHD boot of Cloudbuilder.vhdx
+.EXAMPLE
+    .\Set-VHDBoot.ps1 -ConfigFile "C:\work\Config.xml"
+.EXAMPLE
+    .\Set-VHDBoot.ps1 -HostPassword "password1!" -VHDXFullPath "C:\mas\CloudBuilder.vhdx" -MasterVHDXFullPath "\\fileserver\AzureStack170225\CloudBuilder.vhdx"
+.EXAMPLE
+    .\Set-VHDBoot.ps1 -HostPassword "password1!" -VHDXFullPath "C:\mas3\CloudBuilder.vhdx" -MasterVHDXFullPath "\\fileserver\AzureStack170225\CloudBuilder.vhdx" -VHDXPath "c:\mas3"
+.NOTES
+   Auther : S.Hiro
+   version: 0.5
+#>
+[CmdletBinding(DefaultParametersetName="Manual")]
+Param
+(
+    # Config XML file path
+    [Parameter(Mandatory=$True,ParameterSetName='File')]
+    [string]$ConfigFile,
+    # Host Password
+    [Parameter(Mandatory=$True,Position=0,ParameterSetName='Manual')]
+    [string]$HostPassword,
+    # cloudbuilder.vhdx path
+    [Parameter(Mandatory=$True,Position=1,ParameterSetName='Manual')]
+    [string]$VHDXFullPath,
+    # cloudbuilder.vhdx folder
+    [Parameter(Mandatory=$True,Position=2,ParameterSetName='Manual')]
+    [string]$MasterVHDXFullPath ,
+    # cloudbuilder.vhdx folder
+    [Parameter(Mandatory=$false,Position=3,ParameterSetName='Manual')]
+    [string]$VHDXPath = 'C:\mas'
+)
+$Error.Clear();
+$ErrorActionPreference = 'Stop';
+
+
+if ($PsCmdlet.ParameterSetName -eq "File")
+{
+    if (!(Test-Path $ConfigFile))
+    {
+        Write-Warning -Message "file not found : $ConfigFile";
+        return;
+    }
+    ## load config file
+    Write-Host "load config file : $ConfigFile" -ForegroundColor Green;
+    [xml]$ConfigXml = Get-Content -Path $ConfigFile -Encoding UTF8;
+    $HostPassword = $ConfigXml.AzureStackConfig.Host.Password;
+    $VHDXFullPath = (Join-Path $ConfigXml.AzureStackConfig.VHDXPath $ConfigXml.AzureStackConfig.VHDXFileName);
+    $VHDXPath = $ConfigXml.AzureStackConfig.VHDXPath;
+    $MasterVHDXFullPath = $ConfigXml.AzureStackConfig.MasterVHDXFullPath;
+}
+
+# Create folder
+Write-Host "Create folder : $VHDXPath" -ForegroundColor Green;
+New-Item $VHDXPath -Type directory -Force | Out-Null;
+
+# Download files
+Write-Host "Download files" -ForegroundColor Green;
+$Uri = 'https://raw.githubusercontent.com/Azure/AzureStack-Tools/master/Deployment/';
+'BootMenuNoKVM.ps1', 'PrepareBootFromVHD.ps1', 'unattend_NoKVM.xml' | ForEach-Object { Invoke-WebRequest ($uri + $_) -OutFile ($VHDXPath + '\' + $_) } ;
+
+# copy CloudBuilder
+Write-Host "Copy vhdx" -ForegroundColor Green;
+Write-Verbose " Source Path : $MasterVHDXFullPath";
+Write-Verbose " Destination Path : $VHDXPath";
+##Copy-Item -Path $MasterVHDXFullPath -Destination $VHDXPath -Force;
+
+# change location
+Write-Host "Change location : $VHDXPath" -ForegroundColor Green;
+Set-Location $VHDXPath;
+Write-Host "Set vhd boot" -ForegroundColor Green;
+.\PrepareBootFromVHD.ps1 -CloudBuilderDiskPath $VHDXFullPath -ApplyUnattend -AdminPassword $HostPassword -Verbose
--- README.md
+++ README.md
@@ -1,1 +1,16 @@
-# AzureStack-Scripts
+### AzureStack-Scripts
+
+公開予定のなかったものなのでかなりのやっつけ仕事
+中身的に出せないところも多いので中身の整理が終わったものから
+
+
+### Get started ###
+
+```download-script
+set-location D:\work
+Install-Module -Name AzureRM -RequiredVersion 1.2.8 -Scope CurrentUser
+Install-Module -Name AzureStack  
+invoke-webrequest https://github.com/Pyromaniaxxx/AzureStack-Scripts/archive/master.zip -OutFile master.zip 
+expand-archive master.zip -DestinationPath . -Force;
+
+```
